151c03bbb85f2212e006bde0d0f08738adfbc802
Luewngier <g217881111@gmail.com>
2024-01-17 23:58:42 +0800

    Add application label

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
+# Label
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
+
 
 window.mainloop()
