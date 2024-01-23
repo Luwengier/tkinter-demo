dcad439719f45fda142f0ee413382ec240bf7a18
Luewngier <g217881111@gmail.com>
2024-01-23 23:47:20 +0800

    Add application button

--- main.py
+++ main.py
@@ -8,23 +8,9 @@ window.minsize(width=500, height=300)
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
 my_label.pack()
 
-
-def add(*args) -> None:
-    """Adds all the numbers in the args together and prints the result"""
-    for n in args:
-        print(n)
-
-
-class Car:
-    def __init__(self, **kw):
-        self.make = kw.get("make")
-        self.model = kw.get("model")
-        self.colour = kw.get("colour")
-
-
-my_car = Car(make="Nissan")
-print(my_car.make)
-print(my_car.model)
+# Button
+button = tkinter.Button(text="Click Me")
+button.pack()
 
 
 window.mainloop()
