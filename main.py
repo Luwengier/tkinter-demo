32edbd336c6e1d6c80cf370ebafec40ae6a32f9c
Luewngier <g217881111@gmail.com>
2024-01-24 00:16:22 +0800

    Bind handler to button

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ window.minsize(width=500, height=300)
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
 my_label.pack()
 
+
+def button_clicked():
+    """This function will be called when the button is clicked"""
+    my_label.config(text="I got clicked")
+
+
 # Button
-button = tkinter.Button(text="Click Me")
+button = tkinter.Button(text="Click Me", command=button_clicked)
 button.pack()
 
 
