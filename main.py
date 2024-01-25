ae6c2d8c0c615ae27e3122cfae941933814a7a69
Luewngier <g217881111@gmail.com>
2024-01-25 23:52:20 +0800

    Add entry input and modify handler

--- main.py
+++ main.py
@@ -11,12 +11,17 @@ my_label.pack()
 
 def button_clicked():
     """This function will be called when the button is clicked"""
-    my_label.config(text="I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
 
 
 # Button
 button = tkinter.Button(text="Click Me", command=button_clicked)
 button.pack()
+
+# Entry
+input = tkinter.Entry(width=10)
+input.pack()
 
 
 window.mainloop()
